7f7b255dcd85228e40d90f40f87a3e867f85518a
Sylvain Leroux <sylvain@chicoree.fr>
2021-02-02 18:54:36 +0100

    Fix opt()

--- test/parser.js
+++ test/parser.js
@@ -4,137 +4,139 @@ const debug = require("debug")("backparse:tests-parser");
 const chai = require("chai");
 const assert = chai.assert;
 
-const { Parser, rule, token, END } = require("../lib/parser");
+const { Grammar, rule, token, opt, END } = require("../lib/parser");
 
 describe("The parser", function() {
-  this.timeout(50);
+  this.timeout(60);
 
   it("should accept recursive grammars", function() {
-    const parser = new Parser("r1");
-    parser.define("r1",
+    const grammar = new Grammar("r1");
+    grammar.define("r1",
       [token("A"), rule("r2"), token("A")],
       [token("C")]
     );
 
-    parser.define("r2",
+    grammar.define("r2",
       [ token("B"), rule("r2") ],
       [ token("B") ]
     );
 
+    const parser = grammar.parser("r1");
     for(let tk of ["A", "B", "B", "A", END]) {
       parser.accept(tk);
     }
     assert.equal(parser._status, "success");
     assert.deepEqual(
-            parser.ast(),
-            {
-              "trace": "R:r1",
-              "children": [
-                {
-                  "trace": "T:A",
-                  "children": []
-                },
-                {
-                  "trace": "R:r2",
-                  "children": [
-                    {
-                      "trace": "T:B",
-                      "children": []
-                    },
-                    {
-                      "trace": "R:r2",
-                      "children": [
-                        {
-                          "trace": "T:B",
-                          "children": []
-                        }
-                      ]
-                    }
-                  ]
-                },
-                {
-                  "trace": "T:A",
-                  "children": []
-                }
-              ]
-            }
+      parser.ast(),
+      {
+        "trace": "R:r1",
+        "children": [
+          {
+            "trace": "T:A",
+            "children": []
+          },
+          {
+            "trace": "R:r2",
+            "children": [
+              {
+                "trace": "T:B",
+                "children": []
+              },
+              {
+                "trace": "R:r2",
+                "children": [
+                  {
+                    "trace": "T:B",
+                    "children": []
+                  }
+                ]
+              }
+            ]
+          },
+          {
+            "trace": "T:A",
+            "children": []
+          }
+        ]
+      }
     );
   });
 
 
   it("should accept mutually recursive grammars", function() {
-    const parser = new Parser("r1");
-    parser.define("r1",
+    const grammar = new Grammar("r1");
+    grammar.define("r1",
       [token("A"), rule("r2")],
       [token("A")]
     );
 
-    parser.define("r2",
+    grammar.define("r2",
       [ token("B"), rule("r1") ],
     );
 
 
+    const parser = grammar.parser("r1");
     for(let tk of ["A", "B", "A", "B", "A", END]) {
       parser.accept(tk);
     }
     assert.equal(parser._status, "success");
     assert.deepEqual(
-            parser.ast(),
-            {
-              "trace": "R:r1",
-              "children": [
-                {
-                  "trace": "T:A",
-                  "children": []
-                },
-                {
-                  "trace": "R:r2",
-                  "children": [
-                    {
-                      "trace": "T:B",
-                      "children": []
-                    },
-                    {
-                      "trace": "R:r1",
-                      "children": [
-                        {
-                          "trace": "T:A",
-                          "children": []
-                        },
-                        {
-                          "trace": "R:r2",
-                          "children": [
-                            {
-                              "trace": "T:B",
-                              "children": []
-                            },
-                            {
-                              "trace": "R:r1",
-                              "children": [
-                                {
-                                  "trace": "T:A",
-                                  "children": []
-                                }
-                              ]
-                            }
-                          ]
-                        }
-                      ]
-                    }
-                  ]
-                }
-              ]
-            }
+      parser.ast(),
+      {
+        "trace": "R:r1",
+        "children": [
+          {
+            "trace": "T:A",
+            "children": []
+          },
+          {
+            "trace": "R:r2",
+            "children": [
+              {
+                "trace": "T:B",
+                "children": []
+              },
+              {
+                "trace": "R:r1",
+                "children": [
+                  {
+                    "trace": "T:A",
+                    "children": []
+                  },
+                  {
+                    "trace": "R:r2",
+                    "children": [
+                      {
+                        "trace": "T:B",
+                        "children": []
+                      },
+                      {
+                        "trace": "R:r1",
+                        "children": [
+                          {
+                            "trace": "T:A",
+                            "children": []
+                          }
+                        ]
+                      }
+                    ]
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      }
     );
   });
 
   it("should follow the longuest sentence", function() {
-    const parser = new Parser("r1");
-    parser.define("r1",
+    const grammar = new Grammar("r1");
+    grammar.define("r1",
       [rule("r2")]
     );
 
-    parser.define("r2",
+    grammar.define("r2",
       [ token("A"), rule("r2") ],
       [ token("B"), rule("r2") ],
       [ token("A") ],
@@ -142,78 +144,79 @@ describe("The parser", function() {
     );
 
 
+    const parser = grammar.parser("r1");
     for(let tk of ["A", "B", "A", "B", "A", END]) {
       parser.accept(tk);
     }
     assert.equal(parser._status, "success");
-    let ast = parser.ast();
     assert.deepEqual(
-            parser.ast(),
-            {
-              "trace": "R:r1",
-              "children": [
-                {
-                  "trace": "R:r2",
-                  "children": [
-                    {
-                      "trace": "T:A",
-                      "children": []
-                    },
-                    {
-                      "trace": "R:r2",
-                      "children": [
-                        {
-                          "trace": "T:B",
-                          "children": []
-                        },
-                        {
-                          "trace": "R:r2",
-                          "children": [
-                            {
-                              "trace": "T:A",
-                              "children": []
-                            },
-                            {
-                              "trace": "R:r2",
-                              "children": [
-                                {
-                                  "trace": "T:B",
-                                  "children": []
-                                },
-                                {
-                                  "trace": "R:r2",
-                                  "children": [
-                                    {
-                                      "trace": "T:A",
-                                      "children": []
-                                    }
-                                  ]
-                                }
-                              ]
-                            }
-                          ]
-                        }
-                      ]
-                    }
-                  ]
-                }
-              ]
-            }
+      parser.ast(),
+      {
+        "trace": "R:r1",
+        "children": [
+          {
+            "trace": "R:r2",
+            "children": [
+              {
+                "trace": "T:A",
+                "children": []
+              },
+              {
+                "trace": "R:r2",
+                "children": [
+                  {
+                    "trace": "T:B",
+                    "children": []
+                  },
+                  {
+                    "trace": "R:r2",
+                    "children": [
+                      {
+                        "trace": "T:A",
+                        "children": []
+                      },
+                      {
+                        "trace": "R:r2",
+                        "children": [
+                          {
+                            "trace": "T:B",
+                            "children": []
+                          },
+                          {
+                            "trace": "R:r2",
+                            "children": [
+                              {
+                                "trace": "T:A",
+                                "children": []
+                              }
+                            ]
+                          }
+                        ]
+                      }
+                    ]
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      }
     );
   });
 
   it("should detect invalid sentences", function() {
-    const parser = new Parser("r1");
-    parser.define("r1",
+    const grammar = new Grammar("r1");
+    grammar.define("r1",
       [token("A"), rule("r2"), token("A")]
     );
 
-    parser.define("r2",
+    grammar.define("r2",
       [ token("B"), rule("r2") ],
       [ token("B") ]
     );
 
 
+    const parser = grammar.parser("r1");
     for(let tk of ["A", "B", "A", "B", "A", END]) {
       parser.accept(tk);
     }
@@ -221,62 +224,176 @@ describe("The parser", function() {
   });
 
   it("should accept epsilon-transitions", function() {
-    const parser = new Parser("r1");
-    parser.define("r1",
+    const grammar = new Grammar("r1");
+    grammar.define("r1",
       [token("A"), rule("r2"), token("A")],
       [token("C")]
     );
 
-    parser.define("r2",
+    grammar.define("r2",
       [ token("B"), rule("r2") ],
       [  ]
     );
 
 
+    const parser = grammar.parser("r1");
     for(let tk of ["A", "B", "B", "A", END]) {
       parser.accept(tk);
     }
     assert.equal(parser._status, "success");
     assert.deepEqual(
-            parser.ast(),
-            {
-              "trace": "R:r1",
-              "children": [
-                {
-                  "trace": "T:A",
-                  "children": []
-                },
-                {
-                  "trace": "R:r2",
-                  "children": [
-                    {
-                      "trace": "T:B",
-                      "children": []
-                    },
-                    {
-                      "trace": "R:r2",
-                      "children": [
-                        {
-                          "trace": "T:B",
-                          "children": []
-                        },
-                        {
-                          "trace": "R:r2",
-                          "children": []
-                        }
-                      ]
-                    }
-                  ]
-                },
-                {
-                  "trace": "T:A",
-                  "children": []
-                }
-              ]
-            }
+      parser.ast(),
+      {
+        "trace": "R:r1",
+        "children": [
+          {
+            "trace": "T:A",
+            "children": []
+          },
+          {
+            "trace": "R:r2",
+            "children": [
+              {
+                "trace": "T:B",
+                "children": []
+              },
+              {
+                "trace": "R:r2",
+                "children": [
+                  {
+                    "trace": "T:B",
+                    "children": []
+                  },
+                  {
+                    "trace": "R:r2",
+                    "children": []
+                  }
+                ]
+              }
+            ]
+          },
+          {
+            "trace": "T:A",
+            "children": []
+          }
+        ]
+      }
     );
   });
 
+  describe("optional path", function() {
+    let grammar;
+
+    beforeEach(function() {
+      grammar = new Grammar("r1");
+      grammar.define("r1",
+        [token("A"), opt(rule("r2")), token("A")],
+        [token("C")]
+      );
+
+      grammar.define("r2",
+        [ token("B") ],
+        [ token("C") ]
+      );
+    });
+
+    it("should accept optional paths (shortcut)", function() {
+      const parser = grammar.parser("r1");
+      for(let tk of ["A", "A", END]) {
+        parser.accept(tk);
+      }
+      console.log(JSON.stringify(parser.ast(), null, 2));
+      assert.equal(parser._status, "success");
+
+      assert.deepEqual(
+        parser.ast(),
+        {
+          "trace": "R:r1",
+          "children": [
+            {
+              "trace": "T:A",
+              "children": []
+            },
+            {
+              "trace": "T:A",
+              "children": []
+            }
+          ]
+        }
+      );
+    });
+
+    it("should accept optional paths (1st)", function() {
+      const parser = grammar.parser("r1");
+      for(let tk of ["A", "B", "A", END]) {
+        parser.accept(tk);
+      }
+      console.log(JSON.stringify(parser.ast(), null, 2));
+      assert.equal(parser._status, "success");
+
+      assert.deepEqual(
+        parser.ast(),
+        {
+          "trace": "R:r1",
+          "children": [
+            {
+              "trace": "T:A",
+              "children": []
+            },
+            {
+              "trace": "R:r2",
+              "children": [
+                {
+                  "trace": "T:B",
+                  "children": []
+                }
+              ]
+            },
+            {
+              "trace": "T:A",
+              "children": []
+            }
+          ]
+        }
+      );
+    });
+
+    it("should accept optional paths (2nd)", function() {
+      const parser = grammar.parser("r1");
+      for(let tk of ["A", "C", "A", END]) {
+        parser.accept(tk);
+      }
+      console.log(JSON.stringify(parser.ast(), null, 2));
+      assert.equal(parser._status, "success");
+
+      assert.deepEqual(
+        parser.ast(),
+        {
+          "trace": "R:r1",
+          "children": [
+            {
+              "trace": "T:A",
+              "children": []
+            },
+            {
+              "trace": "R:r2",
+              "children": [
+                {
+                  "trace": "T:C",
+                  "children": []
+                }
+              ]
+            },
+            {
+              "trace": "T:A",
+              "children": []
+            }
+          ]
+        }
+      );
+
+    });
+  });
 
 });
 

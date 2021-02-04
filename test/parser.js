4d5091644bb4139582084af3b9b8916fefc8c6d3
Sylvain Leroux <sylvain@chicoree.fr>
2021-02-04 15:11:00 +0100

    Add the "several" predicate for 0 to many repetitions

--- test/parser.js
+++ test/parser.js
@@ -4,7 +4,7 @@ const debug = require("debug")("backparse:tests-parser");
 const chai = require("chai");
 const assert = chai.assert;
 
-const { Grammar, rule, token, opt, END } = require("../lib/parser");
+const { Grammar, rule, token, opt, several, END } = require("../lib/parser");
 
 describe("The parser", function() {
   this.timeout(70);
@@ -357,30 +357,131 @@ describe("The parser", function() {
         }
       );
     });
+  });
 
-    it("should accept optional paths (2nd)", function() {
+  describe("kleene star", function() {
+    let grammar;
+
+    beforeEach(function() {
+      grammar = new Grammar("r1");
+      grammar.define("r1",
+        [token("A"), several(rule("r2")), token("A")],
+        [token("C")]
+      );
+
+      grammar.define("r2",
+        [ token("B") ],
+        [ token("C") ]
+      );
+    });
+
+    it("should accept 0 repetitions", function() {
       const parser = grammar.parser("r1");
-      for(let tk of ["A", "C", "A", END]) {
+      for(let tk of ["A", "A", END]) {
         parser.accept(tk);
       }
       console.log(JSON.stringify(parser.ast(), null, 2));
-      console.log(parser.ast());
       assert.equal(parser._status, "success");
+
       assert.deepEqual(
         parser.ast(),
         {
-          "value": "r1",
           "children": [
             {
               "value": "A",
               "children": []
             },
             {
-              "value": "r2",
+              "value": "*",
+              "children": [
+              ]
+            },
+            {
+              "value": "A",
+              "children": []
+            }
+          ],
+          "value": "r1"
+        }
+      );
+    });
+
+    it("should accept 1 repetitions", function() {
+      const parser = grammar.parser("r1");
+      for(let tk of ["A", "B", "A", END]) {
+        parser.accept(tk);
+      }
+      console.log(JSON.stringify(parser.ast(), null, 2));
+      assert.equal(parser._status, "success");
+
+      assert.deepEqual(
+        parser.ast(),
+        {
+          "children": [
+            {
+              "value": "A",
+              "children": []
+            },
+            {
+              "value": "*",
               "children": [
                 {
-                  "value": "C",
-                  "children": []
+                  "value": "r2",
+                  "children": [
+                    {
+                      "value": "B",
+                      "children": []
+                    }
+                  ]
+                },
+              ]
+            },
+            {
+              "value": "A",
+              "children": []
+            }
+          ],
+          "value": "r1"
+        }
+      );
+    });
+
+    it("should accept 2 repetitions", function() {
+      const parser = grammar.parser("r1");
+      for(let tk of ["A", "B", "B", "A", END]) {
+        parser.accept(tk);
+      }
+      console.log(JSON.stringify(parser.ast(), null, 2));
+      assert.equal(parser._status, "success");
+
+      assert.deepEqual(
+        parser.ast(),
+        {
+          "children": [
+            {
+              "value": "A",
+              "children": []
+            },
+            {
+              "value": "*",
+              "children": [
+                {
+                  "value": "r2",
+                  "children": [
+                    {
+                      "value": "B",
+                      "children": []
+                    }
+                  ]
+                },
+                {
+                  "value": "r2",
+                  "children": [
+                    {
+                      "value": "B",
+                      "children": []
+                    }
+                  ]
                 }
               ]
             },
@@ -388,20 +489,67 @@ describe("The parser", function() {
               "value": "A",
               "children": []
             }
-          ]
+          ],
+          "value": "r1"
         }
+
       );
     });
 
-    it("should detect bad token in the optional paths", function() {
-      const parser = grammar.parser("r1");
-      for(let tk of ["A", "D", "A", END]) {
-        parser.accept(tk);
-      }
-      assert.equal(parser._status, "failure");
+    it("should reject bad sentences", function() {
+      this.timeout(1000);
 
+      const testCases = [
+         ["A", "A", "A", END],
+         ["A", "A", "B", END],
+         ["A", "B", "A", "B", END],
+         ["A", "B", "B", "A", "B", END],
+         ["A", "B", "B", "B", "A", "B", END],
+      ];
+
+      for(let testCase of testCases) {
+        const parser = grammar.parser("r1");
+        for(let tk of testCase) {
+          parser.accept(tk);
+        }
+        assert.equal(parser._status, "failure", testCase.join(" "));
+      }
     });
   });
 
+  describe("complex cases", function() {
+
+    it("deep grammar", function() {
+      this.timeout(1000);
+
+      const grammar = new Grammar();
+      grammar.define("r1", [token("A"), opt(rule("r2"))], [token("B"), opt(rule("r2"))]);
+      grammar.define("r2", [token("A"), opt(rule("r3"))], [token("B"), opt(rule("r3"))]);
+      grammar.define("r3", [token("A"), opt(rule("r4"))], [token("B"), opt(rule("r4"))]);
+      grammar.define("r4", [token("A"), opt(rule("r5"))], [token("B"), opt(rule("r5"))]);
+      grammar.define("r5", [token("A"), opt(rule("r6"))], [token("B"), opt(rule("r6"))]);
+      grammar.define("r6", [token("A")], [token("B")]);
+
+      const testCases = [
+         ["A", "A", "A", "A", "A", "A", END],
+         ["A", "A", "A", "A", "A", "B", END],
+         ["A", "A", "A", "A", "B", "A", END],
+         ["A", "A", "A", "B", "A", "A", END],
+         ["A", "A", "B", "A", "A", "A", END],
+         ["A", "B", "A", "A", "A", "A", END],
+         ["B", "A", "A", "A", "A", "A", END],
+         ["B", "B", "B", "B", "B", "B", END],
+      ];
+
+      for(let testCase of testCases) {
+        const parser = grammar.parser("r1");
+        for(let tk of testCase) {
+          parser.accept(tk);
+        }
+        assert.equal(parser._status, "success", testCase.join(" "));
+      }
+    });
+
+  });
 });
 
